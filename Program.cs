05cdde8f8c57d1fe6e6f1461424d6d9d02aeb99c
Ekber Zeynalli <zeynallikbr2@gmail.com>
2024-01-24 13:38:25 +0300

    second

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿namespace Home.task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+          
+        {
+            string Man = " oglu";
+            string Woman = " qizi";
+
+            Console.WriteLine("Adinizi daxil edin");
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Soyadinizi daxil edin");
+            string surname = Console.ReadLine();
+
+            Console.WriteLine("Ata adini daxil edin");
+            string fatherName = Console.ReadLine();
+
+           /* if (fatherName == Man)
+            {
+                Console.WriteLine("oglu");
+            }
+            else
+            {
+                Console.WriteLine("qizi");
+            }*/
+            Console.WriteLine("Cinsiyeti daxil edin");
+            string gender = Console.ReadLine();
+
+            if(gender == Man)
+            {
+                Console.WriteLine(" oglu");
+            }
+            else
+            {
+                Console.WriteLine(" qizi");
+            }
+            Console.WriteLine("Dogum tarixini daxil edin");
+            string Birthday = Console.ReadLine();
+
+
+            Console.WriteLine("Unvani  daxil edin");
+            string adress = Console.ReadLine();
+
+            Console.WriteLine(name +" " + surname + " " + fatherName + gender);
+           // Console.WriteLine(Birthday + " doğulduğunuz üçün hal-hazida 18 yasiniz var" );
+
+        }
+    }
+}
